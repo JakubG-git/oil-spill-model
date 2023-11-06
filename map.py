6b2b5c1d8fe9dd95772e602597c9cad8aa26ec02
Maciej Adamus <adamus.mac@gmail.com>
2023-11-06 22:39:27 +0100

    simple display

--- map.py
+++ map.py
@@ -7,7 +7,7 @@ wysokosc = 600
 okno = pygame.display.set_mode((szerokosc, wysokosc))
 pygame.display.set_caption("mapa")
 
-obraz = pygame.image.load("obraz.jpg")
+obraz = pygame.image.load("obraz.png")
 
 mapa_pikseli = []
 for y in range(wysokosc):
@@ -16,19 +16,16 @@ for y in range(wysokosc):
         kolor = obraz.get_at((x, y))
         wiersz.append(kolor)
     mapa_pikseli.append(wiersz)
-print(mapa_pikseli)
+# print(mapa_pikseli)
 
 gra = True
 while gra:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gra = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            obraz.set_at((x, y), (0,0,0))
 
-        # Obsługa zdarzeń użytkownika, np. kliknięcia myszy
-
-    # Aktualizacja ekranu
-    for y in range(wysokosc):
-        for x in range(szerokosc):
-            pygame.draw.rect(okno, mapa_pikseli[y][x], (x, y, 1, 1))
-
+    okno.blit(obraz, (0, 0))
     pygame.display.update()
