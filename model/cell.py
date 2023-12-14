823873a5e8ca56a33097380251e5e791498572cf
Maciej Adamus <adamus.mac@gmail.com>
2023-12-14 15:54:03 +0100

    podzielono wektory na osobne funkcje

--- model/cell.py
+++ model/cell.py
@@ -9,9 +9,18 @@ class Cell:
         self.water_temp = None
 
     def set_vectors_n_temp(self, wind_vec: (float, float), water_vec: (float, float), water_tmp: float):
-        self.water_temp = water_tmp
-        self.wind_vector = wind_vec
+        self.set_water_temp(water_tmp)
+        self.set_wind_wector(wind_vec)
+        self.set_water_wector(water_vec)
+
+    def set_water_wector(self, water_vec: (float, float)):
         self.water_vector = water_vec
+
+    def set_wind_wector(self, wind_vec: (float, float)):
+        self.wind_vector = wind_vec
+
+    def set_water_temp(self, water_tmp: float):
+        self.water_temp = water_tmp
 
     def __str__(self):
         return f"X: {self.x}, Y: {self.y}, Wind: {self.wind_vector}, Current: {self.water_vector}, Temp: {self.water_temp}"
